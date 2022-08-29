fe927149f9093683a7543efdc7247da8ec209641
Eduard Konovka <ed098ua@gmail.com>
2022-08-29 21:54:31 +0300

    Adds chapter 3

--- js/1.UpAndGoing/2.js
+++ js/1.UpAndGoing/2.js
@@ -1,9 +1,67 @@
-const foo = function (param) {
+const foo1 = function (param) {
   return param * 3;
 };
 
-foo.fn = function (prop) {
+foo1.fn = function (prop) {
   return this(prop) - 2;
 };
 
-console.log('foo.fn(5) = ', foo.fn(5));
+console.log('foo1.fn(5) = ', foo1.fn(5));
+
+// =============================================================================
+
+const url = (() => window.location)();
+console.log(url);
+
+// =============================================================================
+
+function User() {
+  var username, password;
+
+  function doLogin(user, pw) {
+    username = user;
+    password = pw;
+
+    return `${username} ---> ${password}`;
+  }
+
+  var publicAPI = {
+    login: doLogin,
+  };
+
+  return publicAPI;
+}
+
+// создать экземпляр модуля `User`
+var fred = new User();
+
+const fredData = fred.login('fred', '12Battery34!');
+console.log(fredData);
+
+// =============================================================================
+
+function foo2() {
+  console.log(this.bar);
+}
+
+var bar = 'global';
+
+var obj1 = {
+  bar: 'obj1',
+  foo2: foo2,
+};
+
+var obj2 = {
+  bar: 'obj2',
+};
+
+//--------
+
+obj1.foo2(); // "obj1"
+foo2.call(obj2); // "obj2"
+new foo2(); // undefined
+// foo2(); // "global"
+
+// =============================================================================
+
+console.log(document);
