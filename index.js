8a3ae774cfd38923b9f3bb068a95a45738eec54a
Eduard Konovka <ed098ua@gmail.com>
2022-09-08 00:55:17 +0300

    Renames index files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import './js/1.UpAndGoing/UpAndGoing.js';
-import './js/2.ScopeAndClosures/ScopeAndClosures.js';
-import './js/3.thisAndObjectPrototypes/thisAndObjectPrototypes.js';
-import './js/4.TypesAndGrammar/TypesAndGrammar.js';
-import './js/5.AsyncAndPerformance/AsyncAndPerformance.js';
-import './js/6.ES6AndBeyond/ES6AndBeyond.js';
+import './js/1.UpAndGoing/index.js';
+import './js/2.ScopeAndClosures/index.js';
+import './js/3.thisAndObjectPrototypes/index.js';
+import './js/4.TypesAndGrammar/index.js';
+import './js/5.AsyncAndPerformance/index.js';
+import './js/6.ES6AndBeyond/index.js';
