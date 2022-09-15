53b174d78b8e6c5dd6cdfbdb8cd9ef72f1984d23
Eduard Konovka <ed098ua@gmail.com>
2022-09-15 23:20:26 +0300

    Adds rules for defining "this"

--- js/2.ScopeAndClosures/C.js
+++ js/2.ScopeAndClosures/C.js
@@ -1,7 +1,7 @@
 var obj1 = {
   id1: 'awesome 1',
-  cool1: function coolFn1(binding = 'setTimeout') {
-    console.log('id 1 ---> ', this.id1, `\n${binding} this 1 ---> `, this);
+  cool1: function coolFn1(contex = 'setTimeout') {
+    console.log('id 1 ---> ', this.id1, `\n${contex} this 1 ---> `, this);
   },
 };
 
@@ -15,8 +15,8 @@ setTimeout(obj1.cool1, 100); // no awesome 1
 
 var obj2 = {
   id2: 'awesome 2',
-  cool2: function coolFn2(binding = 'setTimeout', self = obj2) {
-    console.log('id 2 ---> ', self.id2, `\n${binding} this 2 ---> `, self);
+  cool2: function coolFn2(contex = 'setTimeout', self = obj2) {
+    console.log('id 2 ---> ', self.id2, `\n${contex} this 2 ---> `, self);
   },
 };
 
@@ -24,4 +24,4 @@ window.id2 = 'not awesome 2';
 
 obj2.cool2('window'); // awesome 2
 
-setTimeout(obj2.cool2, 100); // no awesome 2
+setTimeout(obj2.cool2, 100); // awesome 2
