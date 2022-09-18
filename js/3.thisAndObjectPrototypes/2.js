aa940a01da11b283103a56337fb384f10c425444
Eduard Konovka <ed098ua@gmail.com>
2022-09-18 23:47:54 +0300

    Order of precedence for applying "this" binding rules

--- js/3.thisAndObjectPrototypes/2.js
+++ js/3.thisAndObjectPrototypes/2.js
@@ -45,3 +45,19 @@ function foo2(a) {
 var bar2 = new foo2(2);
 console.log(bar2.a); // 2
 console.log(bar2); // Object { a: 2 }
+
+// =============================================================================
+
+function foo(something) {
+  this.a = something;
+}
+
+var obj1 = {};
+
+var bar = foo.bind(obj1);
+bar(2);
+console.log(obj1.a); // 2
+
+var baz = new bar(3);
+console.log(obj1.a); // 2
+console.log(baz.a); // 3
