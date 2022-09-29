39a61baf0cdc6d242ec0c675561f318e55f398b3
Eduard Konovka <ed098ua@gmail.com>
2022-09-29 20:36:24 +0300

    Adds solution chapter 3.3

--- js/3.thisAndObjectPrototypes/3.js
+++ js/3.thisAndObjectPrototypes/3.js
@@ -112,3 +112,23 @@ console.log(
 ); // ["a", "b"]
 
 // =============================================================================
+
+var randoms = {
+  [Symbol.iterator]: function () {
+    return {
+      next: function () {
+        return { value: Math.random() };
+      },
+    };
+  },
+};
+
+var randoms_pool = [];
+
+for (var n of randoms) {
+  randoms_pool.push(n);
+  // не продолжаем бесконечно!
+  if (randoms_pool.length === 100) break;
+}
+
+console.log('randoms_pool --->', randoms_pool);
