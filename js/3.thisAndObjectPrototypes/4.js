dbebb7f61ce8df022c8f4147c5d518abbb945b02
Eduard Konovka <ed098ua@gmail.com>
2022-10-02 23:53:26 +0300

    Inheriting classes

--- js/3.thisAndObjectPrototypes/4.js
+++ js/3.thisAndObjectPrototypes/4.js
@@ -1,1 +1,15 @@
+class CoolGuy {
+  constructor(trick) {
+    this.specialTrick = trick;
+  }
+
+  showOff() {
+    console.log('Зацените мой трюк: ', this.specialTrick);
+  }
+}
+
+const Joe = new CoolGuy('Прыжок через скакалку');
+
+Joe.showOff(); // Зацените мой трюк: прыжок через скакалку
+
 // =============================================================================
