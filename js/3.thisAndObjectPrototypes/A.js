138e0c868cffca84e29abefa6209f38b7719fa6b
Eduard Konovka <ed098ua@gmail.com>
2022-10-11 14:34:23 +0300

    Adds solution of appendix 3.A

--- js/3.thisAndObjectPrototypes/A.js
+++ js/3.thisAndObjectPrototypes/A.js
@@ -1,1 +1,52 @@
+class Arr {
+  constructor(Name) {
+    this.name = Name;
+    this.num = Math.round(Math.random() * 100);
+  }
+
+  outNameAndNumber() {
+    console.log(`${this.name}: ${this.num}`);
+  }
+}
+
+class ArrExtension extends Arr {
+  constructor(Name, Query, Page) {
+    super(Name);
+    this.query = Query;
+    this.page = Page;
+  }
+
+  outData() {
+    console.log(
+      `${this.name}: ${this.num}\nPage: ${this.page}\nQuery: ${this.query}`,
+    );
+  }
+}
+
+const exemplar1 = new ArrExtension('#1', 'Search', 37);
+
+exemplar1.outNameAndNumber();
+exemplar1.outData();
+
 // =============================================================================
+
+class C {
+  constructor() {
+    this.num = Math.random();
+  }
+  rand() {
+    console.log('Random: ' + this.num);
+  }
+}
+
+var c1 = new C();
+c1.rand(); // "Random: 0.4324299..."
+
+C.prototype.rand = function () {
+  console.log('Random: ' + Math.round(this.num * 1000));
+};
+
+var c2 = new C();
+c2.rand(); // "Random: 867"
+
+c1.rand(); // "Random: 432" -- Ой!!!
