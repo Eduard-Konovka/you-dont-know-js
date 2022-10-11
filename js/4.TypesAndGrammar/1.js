4c126f6457e789bf408341ace0906ad349fd5933
Eduard Konovka <ed098ua@gmail.com>
2022-10-11 15:49:47 +0300

    Adds solution of chapter 4.1

---
+++ js/4.TypesAndGrammar/1.js
@@ -0,0 +1,6 @@
+const DEBUG = true;
+
+// это безопасная проверка на наличие переменной-флага DEBUG
+if (typeof DEBUG !== 'undefined') {
+  console.log('Debugging is starting');
+}
--- js/4.TypesAndGrammar/index.js
+++ js/4.TypesAndGrammar/index.js
@@ -1,1 +1,1 @@
-import './1.js';
+// import './1.js';
